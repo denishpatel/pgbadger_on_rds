28d2abb686d0542bb53f9e93da144e1c3ca5f965
SKannaian <skann29@yahoo.com>
2021-05-14 17:29:40 -0700

    Update run_pgbadger_rds.sh

--- run_pgbadger_rds.sh
+++ run_pgbadger_rds.sh
@@ -6,9 +6,7 @@ usage()
 {
 cat << EOF
 usage: $0 options
-
 This script will downalod the postgres rds log files and generate pgbadger reports
-
 OPTIONS:
    -h      Show this message
    -i      DBInstanceIdentifier 
@@ -55,50 +53,65 @@ fi
 
 
 # environment variables
-AWS_CREDENTIAL_FILE=~/.aws/aws_credential_file
-export AWS_CREDENTIAL_FILE
+#AWS_CREDENTIAL_FILE=~/.aws/aws_credential_file
+#export AWS_CREDENTIAL_FILE
 
 # let's put  date in a variable
-TODAY=`/bin/date +\%Y-\%m-\%d`
-YESTERDAY=`/bin/date -d "1 day ago" +\%Y-\%m-\%d`
+TODAY=$(date '+%Y-%m-%d')
+#YESTERDAY=`/bin/date -d "1 day ago" +\%Y-\%m-\%d`
+YESTERDAY=$(date -d "1 day ago" +\%Y-\%m-\%d)
+
+DOWNLOAD_DATE=''
+
+if [ $IS_CRON -eq 0 ]
+then
+ DOWNLOAD_DATE=$TODAY
+else
+ DOWNLOAD_DATE=$YESTERDAY
+fi
 
 # pgbadger home
-PGBADGER_HOME=/var/www/html/pgbadger_reports/
-mkdir -p $PGBADGER_HOME
+PGBADGER_HOME=/home/ec2-user/pgbadger/pgbadger-report/
 mkdir -p $PGBADGER_HOME/$AWS_INSTANCE 
 
-
-download_and_run_fun() {
+#function starts
+download_rds_logs_and_generate_html() {
 
 #remove file, if exists
-rm -f $PGBADGER_HOME/$AWS_INSTANCE/postgresql.log.$1
+rm -f $PGBADGER_HOME/$AWS_INSTANCE/postgresql.log.$DOWNLOAD_DATE.txt
 
  #describe and downlowd log files for yesterday
-for filename  in $( aws rds describe-db-log-files $AWS_INSTANCE --region $AWS_REGION |awk {'print $2'}|grep $1)
+#describe and downlowd log files for yesterday
+for filename  in $( aws rds describe-db-log-files --db-instance-identifier $AWS_INSTANCE --region $AWS_REGION |grep error/postgresql |grep $DOWNLOAD_DATE  | awk '{gsub("\"","",$2)} {gsub(",","",$2); print $2}' )
 do
-
  echo $filename
- aws rds download-db-log-files $AWS_INSTANCE --region $AWS_REGION --log-file-name $filename >> $PGBADGER_HOME/$AWS_INSTANCE/postgresql.log.$1
+ aws rds download-db-log-file-portion --db-instance-identifier $AWS_INSTANCE --region $AWS_REGION  --starting-token 0 --output text --log-file-name $filename >> $PGBADGER_HOME/$AWS_INSTANCE/postgresql.log.$DOWNLOAD_DATE.txt
+ #cd $PGBADGER_HOME/$AWS_INSTANCE
+ #ls -ltr
 done
 
 # run pgbadger report
-
-/usr/local/bin/pgbadger -p '%t:%r:%u@%d:[%p]:' $PGBADGER_HOME/$AWS_INSTANCE/postgresql.log.$1  -o $PGBADGER_HOME/$AWS_INSTANCE/postgresql.log.$1.html
+cd $PGBADGER_HOME/$AWS_INSTANCE
+#ls -ltr
+pgbadger --p '%t:%r:%u@%d:[%p]:' postgresql.log.$DOWNLOAD_DATE.txt -o postgresql.log.$DOWNLOAD_DATE.html
 
 # remove log file
-
-rm $PGBADGER_HOME/$AWS_INSTANCE/postgresql.log.$1
- 
+rm $PGBADGER_HOME/$AWS_INSTANCE/postgresql.log.$DOWNLOAD_DATE.txt
  return 0
-
 } 
+#function ends
+
+#call the function to download log files and generate pgbadger html file
+download_rds_logs_and_generate_html $DOWNLOAD_DATE
 
 
 # Download log files and run pgbadger report
 
-if [ $IS_CRON -eq 0 ]
-then
- download_and_run_fun $TODAY
-else
-download_and_run_fun $YESTERDAY
-fi
+#if [ $IS_CRON -eq 0 ]
+#then
+# DOWNLOAD_DATE =  $TODAY
+# download_rds_logs_and_generate_html $DOWNLOAD_DATE
+#else
+# DOWNLOAD_DATE =  $YESTERDAY
+# download_rds_logs_and_generate_html $DOWNLOAD_DATE
+#fi
